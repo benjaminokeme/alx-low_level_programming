1beffd9fda4565e74a998f0b89bf8b31b7362609
benjaminokeme <benjaminokeme61@gmail.com>
2023-06-21 02:28:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints out put using thr put function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
